f0691bb56d0c721f072b6c147fdc45962249f4ee
Moshiur <moshi7403@gmail.com>
2022-02-15 12:31:12 -0700

    solution-2 added

--- src/test/java/TestA.java
+++ src/test/java/TestA.java
@@ -18,5 +18,28 @@ public class TestA {
 
         System.out.println(" Solution : 1 ----> : " + rev);
 
+        /**
+         *  Solution-2
+         */
+
+        StringBuffer m = new StringBuffer(str);
+        System.out.println(" Solution : 2 ----> : "+ m.reverse());
+
+
+        /**
+         *  Solution-3
+         */
+
+        char a[] = str.toCharArray();
+        int len2 = a.length; // 4
+        String rev2 = "";
+
+        for (int i=len2-1; i>=0 ; i--){
+
+            rev2 = rev2 + a[i]; //DCBA
+        }
+
+        System.out.println(" Solution : 3 ----> : "+ rev2);
     }
+
 }
