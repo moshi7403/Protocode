ff7ef6a4a30925420c2f21bfc3a3f7434df0a0a4
Moshiur <moshi7403@gmail.com>
2022-02-15 12:27:44 -0700

    added solution-1

--- src/test/java/TestA.java
+++ src/test/java/TestA.java
@@ -1,2 +1,22 @@
 public class TestA {
+
+    public static void main(String[] args) {
+
+
+        /**
+         *  Solution-1
+         */
+
+        String str = "ABCD";
+        int len = str.length(); //4
+        String rev = "";
+
+        for (int i = len - 1; i >= 0; i--) {  // 3 2 1 0 -1
+
+            rev = rev + str.charAt(i);  //DCBA
+        }
+
+        System.out.println(" Solution : 1 ----> : " + rev);
+
+    }
 }
